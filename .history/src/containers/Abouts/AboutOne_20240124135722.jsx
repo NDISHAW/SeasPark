9d5120e25ed4fcd111a29ef993192338be5f615f
ndishaw <ndichumuriithi@gmail.com>
2024-01-24 11:44:56 +0000

    Theme merging

---
+++ .history/src/containers/Abouts/AboutOne_20240124135722.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  Button, 
+  ImageBoxOne, 
+  SecTitle, 
+  SecSubTitle, 
+  InfoMedia
+} from '../../components';
+
+const AboutOne = ({ className }) => (
+  <div className={`position-relative ${className || ""}`}>
+    <div className="about-shape1 d-none d-xl-block">TechBiz</div>
+    <div className="container z-index-common">
+      <div className="row gx-60">
+        <div className="col-lg-6 col-xl-5 mb-50 mb-lg-0">
+          <ImageBoxOne
+            imageOne="images/about/solution.jpg"
+            imageTwo="images/about/trust.jpg"
+            // video="https://www.dropbox.com/s/b4k0p5swok5l8t2/videoplayback.mp4?dl=1"
+          />
+        </div>
+        <div className="col-lg-6 col-xl-7 align-self-center">
+          <SecSubTitle>
+            <i className="fas fa-bring-forward" />
+            Get best Business solution 2024
+          </SecSubTitle>
+          <SecTitle className="h1">
+            Elevate Your Vision with Our Trusted Business Solutions & Consulting
+            – Where Trust Transforms!
+          </SecTitle>
+          <p className="mb-4 mt-1 pb-3">
+            Transforming Futures: Harnessing technology to elevate customer
+            service, drive sales, and streamline business practices. We're your
+            one-stop solution for strategic guidance, enterprise support, custom
+            development, and cutting-edge technologies. Trust our experts to
+            plan, build, and deliver tailored technology solutions for your
+            business challenges.
+          </p>
+          <InfoMedia
+            image="images/icon/tel-1-1.png"
+            title="24 HOURS SERVICE AVAILABLE"
+            info="Call Us: +(666) 888 0000"
+            className="call-media"
+          />
+          <Button path="/about">
+            About Us
+            <i className="far fa-long-arrow-right" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutOne;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import AppRouters from './routes/AppRouters';
+
+import "./assets/fonts/exo/exo.css";
+import "./assets/fonts/fira-sans/fira-sans.css";
+import "slick-carousel/slick/slick.css"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/fontawesome.css";
+import "./assets/scss/style.scss";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AppRouters />
+    </BrowserRouter>
   );
 }
 
