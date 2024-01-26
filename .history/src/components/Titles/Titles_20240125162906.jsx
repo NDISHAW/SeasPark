063b553db79f579e8cbb2f2541b614b019fd6ecb
ndishaw <ndichumuriithi@gmail.com>
2024-01-26 03:42:53 +0000

    Titles structure change

---
+++ .history/src/components/Titles/Titles_20240125162906.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SecSubTitle = ({children, className})=> (
+  h1
+); 
+
+const SecTitle = ({children, className})=> (
+  <h3 className={`sec-title ${className || ''}`}>{children}</h3>
+);
+
+const TitleWrap = ({children, className})=> (  
+  <div className={`title-area ${className || ''}`}>{children}</div>
+);
+
+export {SecSubTitle, SecTitle, TitleWrap}
