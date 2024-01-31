31351b7fd9e931aa72c1a8e432fbd2e388b34d64
ndishaw <ndichumuriithi@gmail.com>
2024-01-31 13:07:09 +0000

    Embeded map in contact page

--- .history/src/containers/Contacts/ContactTwo_20240131160229.jsx
+++ .history/src/containers/Contacts/ContactTwo_20240131160229.jsx
@@ -45,17 +45,7 @@ const ContactTwo = ({ ...restProps }) => (
                     title="Our Office Address"
                     info="Sasio Rd, Off Lunga Lunga Rd, Nairobi-Kenya"
                   />
-                  <>
-                    <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3988.7754520982653!2d36.860516274965654!3d-1.3100420986775236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMcKwMTgnMzYuMiJTIDM2wrA1MSc0Ny4xIkU!5e0!3m2!1sen!2ske!4v1706706096592!5m2!1sen!2ske"
-                      width="400"
-                      height="200"
-                      // style={{border:0}}
-                      allowfullscreen=""
-                      loading="lazy"
-                      referrerpolicy="no-referrer-when-downgrade"
-                    ></iframe>
-                  </>
+                  
                   <InfoMedia
                     icon="far fa-clock"
                     title="Official Work Time"
