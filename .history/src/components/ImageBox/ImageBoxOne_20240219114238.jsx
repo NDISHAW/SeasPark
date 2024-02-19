d78127f84f58425fdf4dfd97e3741bf7106db89a
ndishaw <ndichumuriithi@gmail.com>
2024-02-19 09:27:31 +0000

    Spacing

---
+++ .history/src/components/ImageBox/ImageBoxOne_20240219114238.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {PlayBtn} from '../';
+
+const ImageBoxOne = ({imageOne, imageTwo, video})=> (
+  <div className="img-box">
+    <div className="img-1">
+      <img src={imageOne} alt="imgOne"/>
+    </div>
+    {/* <div className="img-2">
+      <img src={imageTwo} alt="imgTwo"/>
+      <PlayBtn path={video} className="style2 position-center"><i className="fas fa-play"/></PlayBtn>
+    </div> */}
+
+  </div>
+);
+
+export default ImageBoxOne;
---
+++ .history/src/containers/Services/ServiceFour_20240219082814.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Slider from 'react-slick';
+import {ServiceBoxOne} from '../../components';
+
+
+// Service Data
+import services from "../../data/service.json";
+
+const ServiceFour = ({...restProps})=> {
+
+  
+  const settings = {
+    autoplay: false,
+    autoplaySpeed: 8000,
+    infinite: true,
+    arrows: false,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  };
+
+  return (
+    <div {...restProps}>
+      <div className="container">
+        <Slider className="row" {...settings}>
+          {services.services.map((item) => (
+            <ServiceBoxOne
+              key={item.id}
+              className="layout3"
+              path={`/service-details/${item.id}`}
+              bgImage={item.bgShape}
+              icon={item.icon}
+              title={item.title}
+              text={item.text.slice(0, 200)}
+            />
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+}
+
+export default ServiceFour;
